9403f93ed352d69a6bcb0a8b7e815c30e0af0cf6
Wanda <cafepogs@gmail.com>
2023-01-22 08:25:13 +0100

    update localdata.js

--- localdata.js
+++ localdata.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const POST_LIST_FILE = 'postlist.json';
 
 class LocalData {
-    constructor() { }
+    constructor(fileName = POST_LIST_FILE) {
+        this.fileName = fileName;
+     }
 
     guidExists = async (guid) => {
         var content = await this.readPostsFile();
@@ -18,14 +20,14 @@ class LocalData {
 
     readPostsFile = async () => {
         try {
-            if (!fs.existsSync(POST_LIST_FILE)) {
-                fs.writeFileSync(POST_LIST_FILE, "[]");
+            if (!fs.existsSync(this.fileName)) {
+                fs.writeFileSync(this.fileName, "[]");
             }
         } catch (err) {
             console.log("Error creating postlist.json: " + err);
         }
         try {
-            var data = await fs.promises.readFile(POST_LIST_FILE);
+            var data = await fs.promises.readFile(this.fileName);
             var content = JSON.parse(data);
             return content;
         } catch (err) {
@@ -39,20 +41,20 @@ class LocalData {
         this.writePostsToFile(content);
     }
 
-    cleanUpPostList = async () => {
+    cleanUpPostList = async (days = 2) => {
         var content = await this.readPostsFile();
         var postsToKeep = [];
         var date = new Date();
-        var minDate = date.setDate(date.getDate() - 2);
+        var minDate = date.setDate(date.getDate() - days);
         for (var i = 0; i < content.length; i++) {
             if (content[i].date && content[i].date > minDate)
                 postsToKeep.push(content[i]);
         }
-        this.writePostsFile(postsToKeep);
+        this.writePostsToFile(postsToKeep);
     }
 
     writePostsToFile = (postList) => {
-        fs.writeFile(POST_LIST_FILE, JSON.stringify(postList), (err) => {
+        fs.writeFile(this.fileName, JSON.stringify(postList), (err) => {
             if (err)
                 console.error("Error writing posts to file: " + err);
         });
@@ -62,5 +64,3 @@ class LocalData {
 }
 
 module.exports = LocalData;
-
-//(new LocalData()).guidExists("test");
